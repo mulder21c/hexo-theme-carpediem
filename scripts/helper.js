0036f71fd69b7a239bc05f866dd89c9f9c7d983d
mulder21c <mulder21c@gmail.com>
2022-02-13 15:56:03 +0900

    chore(helper): added a function to handle the postposition for Korean
    it use `josa` module

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,5 +1,10 @@
 const dateFormat = require("date-fns/format");
+const { getJosaPicker } = require("josa");
 
 hexo.extend.helper.register(`dateFormat`, function (...arguments) {
   return dateFormat.call(null, ...arguments);
+});
+
+hexo.extend.helper.register(`getJosa`, (word, josa) => {
+  return getJosaPicker(josa)(word);
 });
