f80a1d8a56f2455a9bee3528dbdb247884489098
mulder21c <mulder21c@gmail.com>
2022-03-14 23:04:28 +0900

    feat(head): added mixin for preload
    The representativeImage was authored to return the dimensions that should be needed in the future.

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,5 +1,8 @@
 const dateFormat = require("date-fns/format");
 const { getJosaPicker } = require("josa");
+const probe = require("probe-image-size");
+const fs = require("fs");
+const path = require("path");
 
 hexo.extend.helper.register(`dateFormat`, function (...arguments) {
   return dateFormat.call(null, ...arguments);
@@ -7,4 +10,34 @@ hexo.extend.helper.register(`dateFormat`, function (...arguments) {
 
 hexo.extend.helper.register(`getJosa`, (word, josa) => {
   return getJosaPicker(josa)(word);
+});
+
+hexo.extend.helper.register(`full_url`, (url) => {
+  if (!url) return;
+  const { origin, protocol } = new URL(hexo.config.url);
+  const { url_for } = hexo.extend.helper.store;
+
+  if (
+    /^(https?|file|ftps?|mailto|javascript|data:image\/[^;]{2,9};):/i.test(url)
+  ) {
+    return url;
+  }
+  if (url.substring(0, 2) == "//") {
+    url = url_for.call(hexo, url);
+    return protocol + url;
+  }
+
+  return origin + url;
+});
+
+hexo.extend.helper.register(`representativeImage`, function () {
+  const { page, theme } = this;
+  const hero = page?.hero || theme?.hero;
+  if (!hero) return null;
+  const filePath = path.join(__dirname, `../../../source`, hero?.url);
+  const dimention = probe.sync(fs.readFileSync(filePath));
+  return {
+    path: hero?.url,
+    ...dimention,
+  };
 });
