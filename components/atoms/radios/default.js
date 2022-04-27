e84f99140854bcaf6a7a885cd45d19e89a220ad0
mulder21c <mulder21c@gmail.com>
2022-04-27 18:26:25 +0900

    refactor(radio): change function name and remove unused code

--- components/atoms/radios/default.js
+++ components/atoms/radios/default.js
@@ -2,13 +2,13 @@
   const { themePrefix: prefix } = GLOBAL;
   const fakeRadioSelector = `span.${prefix}-radio__label`;
 
-  function handleClickCheckbox(event) {
+  function handleClickRadio(event) {
     const { target } = event;
     if (!target.matches(fakeRadioSelector)) return;
     const control = target.previousElementSibling;
-    control.checked = !control.checked;
+    control.checked = true;
     control.focus();
   }
 
-  document.addEventListener(`click`, handleClickCheckbox, false);
+  document.addEventListener(`click`, handleClickRadio, false);
 })();
