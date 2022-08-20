6d7a4ca5f9d7bd3342c14e24c49bd476c853bbeb
mulder21c <mulder21c@gmail.com>
2022-08-21 02:03:51 +0900

    chore(pug-doc): seperate pug-doc by categories of component
    the error occured because stream too large....

--- generate-document.js
+++ generate-document.js
@@ -24,72 +24,133 @@ const iconsMeta = (() => {
     return {};
   }
 })();
+const locals = {
+  page: {},
+  config: jsYml.load(
+    fs.readFileSync(path.resolve(__dirname, `../../_config.yml`), `utf8`)
+  ),
+  theme: jsYml.load(
+    fs.readFileSync(path.resolve(__dirname, `./_config.yml`), `utf8`)
+  ),
+  site: {
+    propTypes,
+  },
+  url: `URL`,
+  is_home: () => true,
+  is_post: () => false,
+  is_archive: () => false,
+  is_category: () => false,
+  is_tag: () => false,
+  url_for: (url) => url,
+  open_graph: (obj) => `
+    <meta property="og:title" content="Hexo">
+    <meta property="og:url" content="http://example.com/">
+    <meta property="og:site_name" content="Hexo">
+    <meta property="og:locale" content="ko_KR">
+    <meta property="article:author" content="John Doe">
+    <meta name="twitter:card" content="summary">
+    ...
+  `,
+  representativeImage: () => ({
+    path: `imgSrc`,
+    width: 100,
+    height: 100,
+    wUnits: "px",
+    hUnits: "px",
+  }),
+  full_url: (url) => url,
+  getIconCategory: function (icon) {
+    const [name, category] = icon.split("/");
+    const { styles } = iconsMeta?.[name] || { styles: [] };
+    const [style] = styles;
+
+    if (category && !styles.includes(category)) {
+      console.warn(
+        "\x1b[33m%s\x1b[0m",
+        `⚠ Cannot find "${icon}" icon from fontawesome.`
+      );
+    }
+
+    return {
+      iconName: name,
+      iconCategory: category || style,
+    };
+  },
+  generateUid: () => uid(),
+  dateFormat,
+  _p: (str) =>
+    ({
+      "label.category": "category",
+    }[str]),
+};
 
 pugDocGen({
-  input: path.resolve(rootPath, "./components/**/*.pug"),
-  output: "./data.json",
-  locals: {
-    page: {},
-    config: jsYml.load(
-      fs.readFileSync(path.resolve(__dirname, `../../_config.yml`), `utf8`)
-    ),
-    theme: jsYml.load(
-      fs.readFileSync(path.resolve(__dirname, `./_config.yml`), `utf8`)
-    ),
-    site: {
-      propTypes,
-    },
-    url: `URL`,
-    is_home: () => true,
-    is_post: () => false,
-    is_archive: () => false,
-    is_category: () => false,
-    is_tag: () => false,
-    url_for: (url) => url,
-    open_graph: (obj) => `
-      <meta property="og:title" content="Hexo">
-      <meta property="og:url" content="http://example.com/">
-      <meta property="og:site_name" content="Hexo">
-      <meta property="og:locale" content="ko_KR">
-      <meta property="article:author" content="John Doe">
-      <meta name="twitter:card" content="summary">
-      ...
-    `,
-    representativeImage: () => ({
-      path: `imgSrc`,
-      width: 100,
-      height: 100,
-      wUnits: "px",
-      hUnits: "px",
-    }),
-    full_url: (url) => url,
-    getIconCategory: function (icon) {
-      const [name, category] = icon.split("/");
-      const { styles } = iconsMeta?.[name] || { styles: [] };
-      const [style] = styles;
-
-      if (category && !styles.includes(category)) {
-        console.warn(
-          "\x1b[33m%s\x1b[0m",
-          `⚠ Cannot find "${icon}" icon from fontawesome.`
-        );
-      }
-
-      return {
-        iconName: name,
-        iconCategory: category || style,
-      };
-    },
-    generateUid: () => uid(),
-    dateFormat,
-  },
+  input: path.resolve(rootPath, "./components/atoms/**/*.pug"),
+  output: "./data-atoms.json",
+  locals,
   complete: function () {
     const stream = new PugDocMD({
-      output: path.resolve(rootPath, "./docs/pug/document.md"),
-      input: path.resolve(rootPath, "./data.json"),
+      output: path.resolve(rootPath, "./docs/pug/atoms.md"),
+      input: path.resolve(rootPath, "./data-atoms.json"),
     });
     stream.on("complete", function () {
-      fs.unlink(path.resolve(rootPath, "./data.json"), () => {});
+      fs.unlink(path.resolve(rootPath, "./data-atoms.json"), () => {});
+    });
+  },
+});
+pugDocGen({
+  input: path.resolve(rootPath, "./components/molecules/**/*.pug"),
+  output: "./data-molecules.json",
+  locals,
+  complete: function () {
+    const stream = new PugDocMD({
+      output: path.resolve(rootPath, "./docs/pug/molecules.md"),
+      input: path.resolve(rootPath, "./data-molecules.json"),
+    });
+    stream.on("complete", function () {
+      fs.unlink(path.resolve(rootPath, "./data-molecules.json"), () => {});
+    });
+  },
+});
+pugDocGen({
+  input: path.resolve(rootPath, "./components/organisms/**/*.pug"),
+  output: "./data-organisms.json",
+  locals,
+  complete: function () {
+    const stream = new PugDocMD({
+      output: path.resolve(rootPath, "./docs/pug/organisms.md"),
+      input: path.resolve(rootPath, "./data-organisms.json"),
+    });
+    stream.on("complete", function () {
+      fs.unlink(path.resolve(rootPath, "./data-organisms.json"), () => {});
+    });
+  },
+});
+pugDocGen({
+  input: path.resolve(rootPath, "./components/templates/**/*.pug"),
+  output: "./data-templates.json",
+  locals,
+  complete: function () {
+    const stream = new PugDocMD({
+      output: path.resolve(rootPath, "./docs/pug/templates.md"),
+      input: path.resolve(rootPath, "./data-templates.json"),
+    });
+    stream.on("complete", function () {
+      fs.unlink(path.resolve(rootPath, "./data-templates.json"), () => {});
+    });
+  },
+});
+pugDocGen({
+  input: path.resolve(rootPath, "./components/utils/**/*.pug"),
+  output: "./data-utils.json",
+  locals,
+  complete: function () {
+    const stream = new PugDocMD({
+      output: path.resolve(rootPath, "./docs/pug/utils.md"),
+      input: path.resolve(rootPath, "./data-utils.json"),
+    });
+    stream.on("complete", function () {
+      fs.unlink(path.resolve(rootPath, "./data-utils.json"), () => {});
     });
   },
 });
