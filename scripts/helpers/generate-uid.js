52424008bec56e3d92efeb8b5338d9c81f73b9d6
mulder21c <mulder21c@gmail.com>
2022-09-25 14:58:44 +0900

    refactor(helper): fixedup generating uid module
    - rewritten to start with the aplpabet since it creates an id that starts with a number

--- scripts/helpers/generate-uid.js
+++ scripts/helpers/generate-uid.js
@@ -1,11 +1,14 @@
-const uid = require("easy-uid");
-
 /**
  * @desc Generate unique id
  * @return {string}
  */
 function generateUidHelper() {
-  return uid();
+  const chars = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`;
+  const prefix = chars[Math.floor(Math.random() * chars.length)];
+  const phase1 = performance.now().toString(36);
+  const phase2 = Math.random().toString(32).substring(2);
+
+  return `${prefix}${phase1}${phase2}`.replace(/\./, "-");
 }
 
 module.exports = generateUidHelper;
