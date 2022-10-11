b344918b5a91a88e0e9bb80956d5f7b53c83a2b1
mulder21c <mulder21c@gmail.com>
2022-10-11 19:17:35 +0900

    feat(theme): added common feature configuring theme

---
+++ components/layouts/common.js
@@ -0,0 +1,46 @@
+import { ThemeSetting } from "../utils/lib";
+
+((window, document) => {
+  const applyTheme = ({ type, value }) => {
+    const setting = {
+      scheme: {
+        prop: `colorScheme`,
+        initial: `auto`,
+      },
+      size: {
+        prop: `fontSize`,
+        initial: `medium`,
+      },
+    };
+
+    ThemeSetting[setting[type]["prop"]] = value;
+    if (value === setting[type]["initial"]) {
+      document.documentElement.removeAttribute(`data-${type}`);
+    } else {
+      document.documentElement.setAttribute(`data-${type}`, value);
+    }
+  };
+
+  const configureTheme = ({ options = [], type, initVal }) => {
+    [...options].forEach((el) => {
+      if (el.value === initVal) {
+        el.checked = true;
+        applyTheme({ type, value: initVal });
+      }
+      el.addEventListener(`change`, ({ target: { value = `` } }) =>
+        applyTheme({ type, value })
+      );
+    });
+  };
+
+  configureTheme({
+    options: document.querySelectorAll(`[name="theme"]`),
+    type: `scheme`,
+    initVal: ThemeSetting.colorScheme,
+  });
+  configureTheme({
+    options: document.querySelectorAll(`[name="font-size"]`),
+    type: `size`,
+    initVal: ThemeSetting.fontSize,
+  });
+})(window, document);
