30537b7637774899975e725b6b1fdf5dfede3a29
mulder21c <mulder21c@gmail.com>
2022-10-30 16:51:34 +0900

    feat(component): added timeline component for archives

--- scripts/locals/index.js
+++ scripts/locals/index.js
@@ -1,4 +1,25 @@
 const vanillaPropTypes = require("vanilla-prop-types");
+const { PropTypes } = vanillaPropTypes;
 
 // provide vanillaPropTypes into `site`
 hexo.locals.set("propTypes", vanillaPropTypes);
+hexo.locals.set("propTypesPreset", {
+  headingLevel: PropTypes.number.custom(({ prop }) => {
+    if (Number.isInteger(prop) && prop > 0 && prop < 7) {
+      return true;
+    } else {
+      throw new Error("level must be an integer between 1 and 6.");
+    }
+  }),
+  heading: PropTypes.shape({
+    level: PropTypes.number.custom(({ prop }) => {
+      if (Number.isInteger(prop) && prop > 0 && prop < 7) {
+        return true;
+      } else {
+        throw new Error("level must be an integer between 1 and 6.");
+      }
+    }).isRequired,
+    htmlString: PropTypes.string,
+    visible: PropTypes.bool,
+  }),
+});
