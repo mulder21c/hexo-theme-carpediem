57138efe8aeab08d7c02c9e094b2eb0b6a62e475
mulder21c <mulder21c@gmail.com>
2022-11-04 23:17:24 +0900

    refactor(util): remove uppercase letters from uid's prefix

--- scripts/helpers/generate-uid.js
+++ scripts/helpers/generate-uid.js
@@ -3,10 +3,10 @@
  * @return {string}
  */
 function generateUidHelper() {
-  const chars = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`;
-  const prefix = chars[Math.floor(Math.random() * chars.length)];
-  const phase1 = performance.now().toString(36);
-  const phase2 = Math.random().toString(32).substring(2);
+  const chars = `abcdefghijklmnopqrstuvwxyz`;
+  const prefix = chars[Math.floor(Math.random() * 46656) % chars.length];
+  const phase1 = performance.now().toString(36).slice(-5);
+  const phase2 = (Math.random() * 46656).toString(32).slice(-5);
 
   return `${prefix}${phase1}${phase2}`.replace(/\./, "-");
 }
