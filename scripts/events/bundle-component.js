f2eb85768696e24eab761ad0205373c92800ef95
mulder21c <mulder21c@gmail.com>
2023-01-08 18:05:58 +0900

    refactor(bundler): refactored component bundler
    - remove detecting file change to re-build, added addProcessor instead
    - added ignore path

--- scripts/events/bundle-component.js
+++ scripts/events/bundle-component.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const glob = require("fast-glob");
 const path = require("path");
 const rollup = require("./libs/rollup");
 const { cleanUpFiles } = require("./libs/cleanup");
@@ -7,6 +6,7 @@ const { themeRoot, themeSourcePath, bundleName } = require("../constants");
 const sourcePath = [
   path.resolve(themeRoot, `./components/**/*.js`),
   `!${path.resolve(themeRoot, `./components/**/lib.js`)}`,
+  `!${path.resolve(themeRoot, `./components/vendor/**/*.js`)}`,
 ];
 const outputPath = path.resolve(themeSourcePath, "./js/");
 
@@ -17,83 +17,42 @@ const outputPath = path.resolve(themeSourcePath, "./js/");
 function bundler() {
   const hexo = this;
   const isDevServer = hexo.env.cmd === "server";
-  const isGenerateStage = hexo.env.cmd === "generate";
-  /**
-   * determine whether to proceed with bundling
-   */
-  const isChanged = (() => {
-    const filePath = path.resolve(outputPath, bundleName);
-    const mapPath = path.resolve(outputPath, `${bundleName}.map`);
 
-    const existsJS = fs.existsSync(filePath);
-    const existsMap = fs.existsSync(mapPath);
-
-    // consider it changed, when
-    // not exists bundle file, run generate command,
-    // or not exists map in dev server
-    if (!existsJS || isGenerateStage || (!existsMap && isDevServer)) {
-      return true;
-    }
-
-    // compare the mtime between bundled file and source files
-    const { mtime: currMtime } = fs.statSync(filePath);
-    return Boolean(
-      glob
-        .sync(path.resolve(themeRoot, `./components/**/*.js`))
-        .find(
-          (file) => fs.statSync(file)?.mtime.getTime() > currMtime.getTime()
-        )
-    );
-  })();
-
-  if (isChanged) {
-    cleanUpFiles([
+  if (!isDevServer) {
+    cleanUpFiles([path.resolve(outputPath, `./${bundleName}.map`)]);
+  }
+  return rollup({
+    rollupOption: {
+      input: sourcePath,
+      output: bundleName,
+    },
+    isDevServer,
+  }).then((result) => {
+    const { code, map } = result;
+    // write bundled file
+    fs.writeFileSync(
       path.resolve(outputPath, `./${bundleName}`),
-      path.resolve(outputPath, `./${bundleName}.map`),
-    ]);
-    return rollup({
-      rollupOption: {
-        input: sourcePath,
-        output: bundleName,
-      },
-      isDevServer,
-    }).then((result) => {
-      const { code, map } = result;
-      // write bundled file
+      map ? `${code}\n//# sourceMappingURL=/js/${bundleName}.map` : `${code}`,
+      {
+        encoding: `utf8`,
+        flag: `w`,
+      }
+    );
+
+    // write map file
+    map &&
       fs.writeFileSync(
-        path.resolve(outputPath, `./${bundleName}`),
-        map ? `${code}\n//# sourceMappingURL=/js/${bundleName}.map` : `${code}`,
+        path.resolve(outputPath, `./${bundleName}.map`),
+        map.toString(),
         {
           encoding: `utf8`,
           flag: `w`,
         }
       );
 
-      // write map file
-      map &&
-        fs.writeFileSync(
-          path.resolve(outputPath, `./${bundleName}.map`),
-          map.toString(),
-          {
-            encoding: `utf8`,
-            flag: `w`,
-          }
-        );
-
-      hexo.log.info(`Successfully Component's JavaScript Bundled`);
-
-      hexo.theme.addProcessor(`components`, function (file) {
-        if (file.type === `update`) {
-          hexo.log.info(
-            `Component's JavaScript is Updated. It started re-bundling`
-          );
-          require(`./bundle-component`).bind(hexo)();
-        }
-      });
-
-      hexo.extend.filter.unregister(`before_generate`, bundler);
-    });
-  }
+    hexo.log.info(`Successfully Component's JavaScript Bundled`);
+    hexo.extend.filter.unregister(`before_generate`, bundler);
+  });
 }
 
 module.exports = bundler;
--- scripts/events/index.js
+++ scripts/events/index.js
@@ -3,3 +3,10 @@ const filter = hexo.extend.filter;
 filter.register(`after_init`, require(`./hero-info-remover`));
 filter.register(`before_generate`, require(`./bundle-component`));
 filter.register(`before_generate`, require(`./hero-info-generator`));
+
+hexo.theme.addProcessor(`components`, function (file) {
+  if (file.path.endsWith(`.js`) && file.type === `update`) {
+    hexo.log.info(`Component's JavaScript is Updated. It started re-bundling`);
+    require(`./bundle-component`).bind(hexo)();
+  }
+});
