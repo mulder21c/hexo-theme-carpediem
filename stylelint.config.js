b717301749e58fe8bb20db22699bf6172605af78
mulder21c <mulder21c@gmail.com>
2023-03-29 20:30:01 +0900

    chore(stylelint): remove stylelint-config-prettier since it is deprecated

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   customSyntax: "postcss-html",
-  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  extends: ["stylelint-config-standard"],
   rules: {},
 };
