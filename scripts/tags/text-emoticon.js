8eaf9aeba933fc4fef3f5aa19e0ebed49d79d193
mulder21c <mulder21c@gmail.com>
2023-04-23 17:29:49 +0900

    feat(tags): added text emoticon tag feature

---
+++ scripts/tags/text-emoticon.js
@@ -0,0 +1,46 @@
+const { htmlTag } = require("hexo-util");
+const parse = require("../utils/parse-custom-tag-param");
+
+/**
+ * @public
+ * @desc generate accessible text emoticon
+ * @alias txt_emo
+ * @property {string} label visible text
+ * @property {string} accName the accessible name
+ * @syntax
+ * {% txt_emo content accName %}
+ * @example
+ * {% txt_emo "= _=a" "긁적긁적" %}
+ */
+function textEmoticonTag(ctx) {
+  const log = ctx?.log || console;
+
+  return function ([label, accName]) {
+    if (!accName) {
+      log.error(`The accName is missed.`);
+      return;
+    }
+
+    const accessibleHiddenContent = htmlTag(
+      `span`,
+      {
+        "aria-hidden": "true",
+        class: "txt-emoticon",
+      },
+      label,
+      false
+    );
+
+    return htmlTag(
+      `span`,
+      {
+        role: "image",
+        "aria-label": accName,
+      },
+      accessibleHiddenContent,
+      false
+    );
+  };
+}
+
+module.exports = textEmoticonTag;
